06c9c7d1ed9ab8b2a335dcd94fc5c62ae3c3c0bf
Shawn Kohltfarber <shawn.kohltfarber@gmail.com>
2020-07-29 13:08:22 -0500

    Updates

---
+++ precache-manifest.855b5d35f6fef36a0dd5eb3a18b78233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d1df8eb9b177c1313a40782a16a04e",
+    "url": "/githubapi/index.html"
+  },
+  {
+    "revision": "56fbc3a830db53cb9b0e",
+    "url": "/githubapi/static/css/main.28bf3706.chunk.css"
+  },
+  {
+    "revision": "86e11d4b796b528d9bd0",
+    "url": "/githubapi/static/js/2.c22b935e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/githubapi/static/js/2.c22b935e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fbc3a830db53cb9b0e",
+    "url": "/githubapi/static/js/main.36bb38b5.chunk.js"
+  },
+  {
+    "revision": "3632bc00f05b7eb21f09",
+    "url": "/githubapi/static/js/runtime-main.ea978f79.js"
+  }
+]);
